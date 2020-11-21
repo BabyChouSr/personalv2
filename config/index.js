53f8135ce85e4ecaf052129c739aee79b15a637c
BabyChouSr <emu4yolife@gmail.com>
2020-11-20 17:40:27 -0800

    first commit

---
+++ config/index.js
@@ -0,0 +1,69 @@
+module.exports = {
+
+    author: "@konstantinmuenster",
+    siteTitle: "Gatsby Starter Portfolio Minimal",
+    siteShortTitle: "Christopher Chou", // Used as logo text in header, footer, and splash screen
+    siteDescription: "A modern one-page portfolio with a clean yet expressive design.",
+    siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+    siteLanguage: "en_US",
+    siteIcon: "content/favicon.png", // Relative to gatsby-config file
+    seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+
+    splashScreen: false, // Set this to true if you want to use the splash screen
+
+    // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+    // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+    
+    // There are icons available for the following platforms: 
+    // Medium, GitHub, LinkedIn, XING, Behance
+    socialMedia: [
+        {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/christopher-chou-b346aa1b0/"
+        },
+        {
+            name: "Blog",
+            url: "http://cswithchris.com/"
+        },
+        {
+            name: "Github",
+            url: "https://github.com/BabyChouSr"
+        },
+        // {
+        //     name: "Behance",
+        //     url: "https://www.behance.net/konstanmnster"
+        // },
+    ],
+  
+    navLinks: {
+        menu: [
+            {
+                name: "Articles",
+                url: "/#articles",
+            },
+            {
+                name: "About Me",
+                url: "/#about",
+            },
+            {
+                name: "Projects",
+                url: "/#projects",
+            },
+        ],
+        button: {
+            name: "Contact",
+            url: "/#contact",
+        }
+    },
+
+    footerLinks: [
+        {
+            name: "Privacy",
+            url: "/privacy"
+        },
+        {
+            name: "Imprint",
+            url: "/imprint"
+        }
+    ]
+}
